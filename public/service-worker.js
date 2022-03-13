f42eb1773a269d4ea0aeaf10f3495270565e3db6
Seeky <seek916@gmail.com>
2022-03-13 16:22:46 -0700

    cant figure it out for heroku finished product

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 const APP_PREFIX = 'PWA-BudgetTracker-';
 const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
     './index.html',
@@ -8,20 +8,13 @@ const FILES_TO_CACHE = [
     './js/index.js',
     './js/idb.js',
     './manifest.json',
-    './icons/icon-512x512.png',
-    './icons/icon-384x384.png',
-    './icons/icon-192x192.png',
-    './icons/icon-152x152.png',
-    './icons/icon-144x144.png',
-    './icons/icon-128x128.png',
-    './icons/icon-96x96.png',
-    './icons/icon-72x72.png'
+    
 ];
 
 
 self.addEventListener('install', function (evnty) {
     evnty.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
+      caches.open(DATA_CACHE_NAME).then(function (cache) {
           console.log("Your files were successfull!");
           return cache.addAll(FILES_TO_CACHE)
       })
@@ -34,7 +27,7 @@ self.addEventListener('activate', function (evnty) {
           let cacheKeeplist = keyList.filter(function (key) {
               return key.indexOf(APP_PREFIX);
           });
-          cacheKeeplist.push(CACHE_NAME);
+          cacheKeeplist.push(DATA_CACHE_NAME);
 
           return Promise.all(keyList.map(function (key, i) {
               if (cacheKeeplist.indexOf(key) === -1) {
