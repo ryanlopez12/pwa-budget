16a364b689eea7cf4d5f5d5bfd2e3bf5dbe6878f
Seeky <seek916@gmail.com>
2022-03-13 14:18:48 -0700

    server edit, created git

--- Develop/public/js/indexdb.js
+++ Develop/public/js/indexdb.js
@@ -1,6 +1,6 @@
 let db;
 
-const request = indexedDB.open("budget-tracker", 1);
+const request = indexedDB.open("PWA-budgetTracker-", 1);
 
 request.onupgradeneeded = function (event) {
   const db = event.target.result;
